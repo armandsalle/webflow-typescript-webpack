2bbc0a2307a0d01227342eaca8b6fece81e8fe64
armandsalle <armand.salle@gmail.com>
2021-09-12 13:00:22 +0200

    remove(lodash): remove lodash fill

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
 import $ from 'jquery'
-import { fill } from 'lodash'
 
 import { doubleNumber } from './test'
-
-const b = fill(Array(4), 'lol')
 
 console.log('hello world')
 
 console.log($('.my-element'))
 console.log(doubleNumber(9))
-console.log(b)
