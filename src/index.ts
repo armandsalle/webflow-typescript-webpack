7ad67a752241af92689174a40266da73daead414
armandsalle <armand.salle@gmail.com>
2021-09-12 12:50:56 +0200

    fix(double): rename

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import $ from 'jquery'
 import { fill } from 'lodash'
 
-import { toto } from './test'
+import { doubleNumber } from './test'
 
 const b = fill(Array(4), 'lol')
 
 console.log('hello world')
 
 console.log($('.my-element'))
-console.log(toto(9))
+console.log(doubleNumber(9))
 console.log(b)
