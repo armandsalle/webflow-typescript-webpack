8f93cbe8031b3871dbe3d91160efda7367b01806
armandsalle <armand.salle@gmail.com>
2021-09-28 16:28:17 +0200

    fix(webpack): entry configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,19 +5,37 @@
 const path = require('path')
 
 const config = {
+  /*
+    With this setup, you need to register your TS files by giving them a name and a path.
+    The output will be the same structure as the entry.
+
+    It's a nice pattern if you have only a few pages with a lot of logic. You link the JS file for the page that need JS.
+
+    e.g:
+     index: './src/index.ts' -> dist/index.js
+     about: './src/about.ts' -> dist/about.js
+
+    But, if you want to load the same JS for each pages, you can convert the entry and the output by:
+    
+      entry: './src/index.ts',
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'app.js',
+        publicPath: '/',
+        clean: true,
+      },
+  */
+  entry: {
+    index: './src/index.ts',
+    about: './src/about.ts',
+  },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/',
     clean: true,
   },
-  // entry: './src/index.ts',
-  // output: {
-  //   path: path.resolve(__dirname, 'dist'),
-  //   filename: 'app.js',
-  //   publicPath: '/',
-  //   clean: true,
-  // },
+
   module: {
     rules: [
       {
